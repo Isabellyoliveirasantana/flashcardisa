6beb908eb90db36fd6b513808dd765db52f98464
Isabelly santana <isabelly.oliveira.santana@escola.pr.gov.br>
2025-05-14 08:41:57 -0300

    projeto

--- README.md
+++
@@ -1,1 +0,0 @@
-# flashcardisa
---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biotecnologia',
+    'O que é biotecnologia?',
+    'A biotecnologia é um campo amplo que usa organismos vivos, ou componentes deles, para desenvolver produtos, processos e tecnologias.'
+)
+
+criaCartao(
+    'Geografia',
+   'Em que continente se encontra o maior deserto do mundo?',
+    'a Antártida, encontra-se no continente antártico'
+)
+
+criaCartao(
+    'Históŕia',
+    'Que país tem o formato de uma bota? ',
+    'A Itália é o país que tem o formato de uma bota'
+)
+
+criaCartao(
+    'Física',
+    'A que temperatura a água ferve?',
+    '100 ºC é conhecido como o ponto de ebulição da água.'
+)
